796cc03f4d2d33425577da98cabce26a927df231
Regina Annemarie Bool <ninisalter@gmail.com>
2025-11-12 16:30:35 +0800

    Created draft of Main.java

---
+++ Main.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello! Welcome to Grab 🛺");
+        System.out.print("New to Grab? Press 'Enter' to Sign Up! ");
+        input.nextLine();
+
+        System.out.println("\nTagline Here"); // edit this, something like "Your Journey Starts Here"
+
+        System.out.print("Name: ");
+        String name = input.nextLine();
+
+        System.out.print("Age: ");
+        int age = input.nextInt();
+        input.nextLine();
+
+        System.out.print("Address: ");
+        String address = input.nextLine();
+
+        System.out.print("Contact Number: ");
+        int contactNumber = input.nextInt();
+        input.nextLine();
+        
+        System.out.println("\nPlease select your Account Type:");
+        System.out.println("1. Passenger");
+        System.out.println("2. Driver\n");
+        System.out.print("Account Type: ");
+        int accountType = input.nextInt();
+        input.nextLine();
+
+        String category = "";
+
+        if (accountType == 1) {
+            System.out.println("\nWelcome to the Ride, Passenger!");
+            System.out.println("Final checks: Are you eligible for any special discounts?");
+            System.out.println("1. No, I'm a Regular Passenger");
+            System.out.println("2. Yes, I'm a Student");
+            System.out.println("3. Yes, I'm a Person with Disability (PWD)");
+            System.out.println("4. Yes, I'm a Senior Citizen");
+            System.out.print("Enter your choice: ");
+            int passengerType = input.nextInt();
+
+            switch (passengerType) {
+                case 1:
+                    category = "Regular";
+                    break;
+                case 2:
+                    category = "Student";
+                    break;
+                case 3:
+                    category = "PWD";
+                    break;
+                case 4:
+                    category = "Senior Citizen";
+                    break;
+                default:
+                    category = "Regular";
+                    break;
+            }
+
+            if (category != "Regular") {
+                System.out.print("\nEnter your Valid ID Number: ");
+                String idNumber = input.nextLine();
+                // Insert verifying loading bar here
+                System.out.println("\n✅ Your " + category + " Valid ID is now verified!");
+            }
+
+            // Loading bar here for transition
+            System.out.println("All set! Let's hit the road, " + name + "!");
+            System.out.print("Press 'Enter' to go to the Home Page! ");
+            input.nextLine();
+        }
+
+        input.close();
+    }
+}
