f3899a49d344dee2f6587e424cd581c557afefb2
Mhalik <mhalikperez@gmail.com>
2025-11-24 21:28:50 +0800

    Optimized User

--- OOP/User.java
+++ OOP/User.java
@@ -1,22 +1,27 @@
-//not yet abstract, hindi pa inaapply ang inheritance
 public abstract class User{
-    protected String name;
-    protected int age;
-    protected String address;
-    protected String contactNumber;
+    private final String name;
+    private final int age;
+    private final String address;
+    private final String contactNumber;
 
     public User(String name, int age, String address, String contactNumber) {
         this.name = name;
-        this.age =  age;
+        this.age = age;
         this.address = address;
         this.contactNumber = contactNumber;
     }
 
-    public void displayInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Address: " + address);
-        System.out.println("Contact: " + contactNumber);
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getAddress() { return address; }
+    public String getContactNumber() { return contactNumber; }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\n" +
+               "Age: " + age + "\n" +
+               "Address: " + address + "\n" +
+               "Contact: " + contactNumber;
     }
 
 }
