ae6862cfed313ae73ad46a15c60515015611e87d
Mhalik <mhalikperez@gmail.com>
2025-11-24 20:40:01 +0800

    Optimized Passenger Class

--- OOP/Passenger.java
+++ OOP/Passenger.java
@@ -1,14 +1,25 @@
+enum UserType {
+    REGULAR,
+    STUDENT,
+    SENIOR,
+    PWD
+}
+
 public class Passenger extends User {
     private String userType;
     private String idNumber;
 
-    public Passenger(String name, int age, String address, String contactNumber, String userType, String idNumber) {
+    public Passenger(String name, int age, String address, String contactNumber, UserType userType, String idNumber) {
         super(name, age, address, contactNumber);
         this.userType = userType;
         this.idNumber = idNumber;
     }
 
-    public String getUserType() { return userType; }
+    public UserType getUserType() { return userType; }
+    public String getIdNumber() { return idNumber; }
+
+    public void setUserType(UserType userType) { this.userType = userType; }
+    public void setIdNumber(String idNumber) { this.idNumber = idNumber; }
 
     @Override
     public void displayInfo() {
