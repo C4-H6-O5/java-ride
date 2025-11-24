e9d1644bf3162b559ee090ce6b29bfc54d5684a4
Mhalik <mhalikperez@gmail.com>
2025-11-24 22:27:36 +0800

    Optimized Driver

--- OOP/Driver.java
+++ OOP/Driver.java
@@ -1,7 +1,7 @@
 public class Driver extends User {
     private Vehicle vehicle;
     private String bio;
-    private double rating;
+    private final double rating;
 
     public Driver(String name, int age, String address, String contactNumber, Vehicle vehicle, String bio, double rating) {
         super(name, age, address, contactNumber);
@@ -11,24 +11,17 @@ public class Driver extends User {
     }
 
     public Vehicle getVehicle() { return vehicle; }
-    public void setVehicle(Vehicle vehicle) { this.vehicle = vehicle; }
-
     public String getBio() { return bio; }
+    public double getRating() { return rating; }
+
+    public void setVehicle(Vehicle vehicle) { this.vehicle = vehicle; }
     public void setBio(String bio) { this.bio = bio; }
 
-    public double getRating() { return rating; }
-    public void setRating(double rating) { this.rating = rating; }
-
     @Override
-    public void displayInfo() {
-        super.displayInfo();
-        if (vehicle != null) {
-            System.out.println("Vehicle: " + vehicle);
-            System.out.println("Vehicle Type: " + vehicle.getVehicleType());
-        } else {
-            System.out.println("Vehicle: None assigned");
-        }
-        System.out.println("Bio: " + bio);
-        System.out.println("Rating: " + rating + "⭐");
+    public String toString() {
+        return super.toString() + "\n" +
+               "Vehicle: " + (vehicle != null ? vehicle.toString() : "None assigned") + "\n" +
+               "Bio: " + bio + "\n" +
+               String.format("Rating: %.1f⭐", rating);
     }
 }
