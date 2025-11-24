4db28e8ff47e39155eef3d5f6f6b2d7ccfa6b087
Mhalik <mhalikperez@gmail.com>
2025-11-24 20:36:16 +0800

    Optimized Driver Class

--- OOP/Driver.java
+++ OOP/Driver.java
@@ -1,23 +1,33 @@
 public class Driver extends User {
-    private String vehicleType;
+    private Vehicle vehicle;
     private String bio;
     private double rating;
 
-    public Driver(String name, int age, String address, String contactNumber, String vehicleType, String bio, double rating) {
+    public Driver(String name, int age, String address, String contactNumber, Vehicle vehicle, String bio, double rating) {
         super(name, age, address, contactNumber);
-        this.vehicleType = vehicleType;
+        this.vehicle = vehicle;
         this.bio = bio;
         this.rating = rating;
     }
 
-    public String getVehicleType() { return vehicleType; }
+    public Vehicle getVehicle() { return vehicle; }
+    public void setVehicle(Vehicle vehicle) { this.vehicle = vehicle; }
 
+    public String getBio() { return bio; }
+    public void setBio(String bio) { this.bio = bio; }
+
+    public double getRating() { return rating; }
     public void setRating(double rating) { this.rating = rating; }
 
     @Override
     public void displayInfo() {
         super.displayInfo();
-        System.out.println("Vehicle Type: " + vehicleType);
+        if (vehicle != null) {
+            System.out.println("Vehicle: " + vehicle);
+            System.out.println("Vehicle Type: " + vehicle.getVehicleType());
+        } else {
+            System.out.println("Vehicle: None assigned");
+        }
         System.out.println("Bio: " + bio);
         System.out.println("Rating: " + rating + "⭐");
     }
