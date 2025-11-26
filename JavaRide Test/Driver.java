b0d49396fb1d80819a3beeb864080813da338fdc
Regina Annemarie Bool <ninisalter@gmail.com>
2025-11-26 23:21:29 +0800

    Added addReview method

--- JavaRide Test/Driver.java
+++ JavaRide Test/Driver.java
@@ -1,21 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Driver extends User {
     private Vehicle vehicle;
     private String bio;
     private final double rating;
+    private final List<Review> reviews;
 
     public Driver(String name, int age, String address, String contactNumber, Vehicle vehicle, String bio, double rating) {
         super(name, age, address, contactNumber);
         this.vehicle = vehicle;
         this.bio = bio;
         this.rating = rating;
+        this.reviews = new ArrayList<>();
     }
 
     public Vehicle getVehicle() { return vehicle; }
     public String getBio() { return bio; }
     public double getRating() { return rating; }
+    public List<Review> getReviews() { return reviews; }
 
     public void setVehicle(Vehicle vehicle) { this.vehicle = vehicle; }
     public void setBio(String bio) { this.bio = bio; }
+    public void addReview(Review review) { this.reviews.add(review); }
 
     @Override
     public String toString() {
